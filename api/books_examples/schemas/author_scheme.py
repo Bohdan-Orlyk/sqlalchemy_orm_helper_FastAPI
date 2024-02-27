775b5432b05c0f7f87c9fe1d9c0a3f554e5c2184
Bohdan-Orlyk <lollikwinner@gmail.com>
2024-02-27 18:06:17 +0100

    refactoring

--- api/books_examples/schemas/author_scheme.py
+++ api/books_examples/schemas/author_scheme.py
@@ -2,7 +2,18 @@ from pydantic import BaseModel
 from api.books_examples.schemas.book_scheme import BookScheme
 
 
-class AuthorScheme(BaseModel):
+class BaseAuthorScheme(BaseModel):
     author_name: str
 
+
+class ReadAuthorScheme(BaseAuthorScheme):
+    author_id: int
+
     books: list[BookScheme]
+
+
+class CreateAuthorScheme(BaseAuthorScheme):
+    pass
+
+
+
--- api/books_examples/schemas/book_scheme.py
+++ api/books_examples/schemas/book_scheme.py
@@ -3,3 +3,5 @@ from pydantic import BaseModel
 
 class BookScheme(BaseModel):
     title: str
+
+
